e1d0d5bd05fe49dff2126068c99d42d5435f2411
Aayush dua <aayushdua@Aayushs-MacBook-Air.local>
2024-05-21 16:29:02 +0530

    s

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -38,8 +38,8 @@ const Cart = () => {
           <div>
             <div className="cart-total-details"><p>Subtotal</p><p>Rs.{getTotalCartAmount()}</p></div>
             <hr />
-            <div className="cart-total-details"><p>Delivery Fee</p><p>Rs.{getTotalCartAmount()===0?0:5}</p></div>
-            <hr />
+            {/* <div className="cart-total-details"><p>Delivery Fee</p><p>Rs.{getTotalCartAmount()===0?0:5}</p></div>
+            <hr /> */}
             <div className="cart-total-details"><b>Total</b><b>Rs.{getTotalCartAmount()===0?0:getTotalCartAmount()+5}</b></div>
           </div>
           <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
